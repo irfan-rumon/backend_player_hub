fd843aaaa542fa733db157312ce7d3c77fe72f1a
irfan-rumon <irfannisho8571@gmail.com>
2024-10-22 16:01:46 +0600

    Add CRUD functionalities at player endpoints

--- player/models.py
+++ player/models.py
@@ -1,3 +1,9 @@
+# models.py
 from django.db import models
 
-# Create your models here.
+class Player(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
---
+++ player/urls.py
@@ -0,0 +1,11 @@
+# urls.py (in the 'api' app directory)
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PlayerViewSet
+
+router = DefaultRouter()
+router.register(r'players', PlayerViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
